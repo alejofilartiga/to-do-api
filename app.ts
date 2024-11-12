2aa874960713365c35f0025547ad8da287c538ce
Alejo <alejofilartiga@gmail.com>
2024-11-12 18:09:24 -0300

    Cors Added

--- app.ts
+++ app.ts
@@ -1,14 +1,20 @@
 import express from "express";
 import { connectDB } from "./database/config";
-import toDoRoutes from './routes/toDoRoutes'
+import toDoRoutes from './routes/toDoRoutes';
+import dotenv from "dotenv";
+import cors from "cors";
 
+dotenv.config();
 
-const app = express()
+const app = express();
 
-connectDB()
+connectDB();
 
-app.use(express.json())
-app.use("/todo",toDoRoutes)
-app.listen(8080,() => {
-  console.log("Servidor iniciado en el puerto 8080")
-})
+app.use(cors());
+app.use(express.json());
+app.use("/todo", toDoRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Servidor iniciado en el puerto", PORT);
+});
