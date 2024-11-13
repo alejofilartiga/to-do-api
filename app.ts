3b20c49288d5c01033a679192a494c5d5545976d
alejofilartiga <alejontu400@gmail.com>
2024-11-12 21:44:01 -0300

    Update app.ts

--- app.ts
+++ app.ts
@@ -2,7 +2,6 @@ import express from "express";
 import { connectDB } from "./database/config";
 import toDoRoutes from './routes/toDoRoutes';
 import dotenv from "dotenv";
-import cors from "cors";
 
 dotenv.config();
 
@@ -10,7 +9,7 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+
 app.use(express.json());
 app.use("/todo", toDoRoutes);
 
