5cedab39a4bfdca5b9325d883ecc1b4c5acf0c9e
Alejo <alejofilartiga@gmail.com>
2024-11-12 22:03:19 -0300

    Cors Fixed

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { connectDB } from "./database/config";
 import toDoRoutes from './routes/toDoRoutes';
 import dotenv from "dotenv";
+import cors from "cors"
 
 dotenv.config();
 
@@ -9,7 +10,7 @@ const app = express();
 
 connectDB();
 
-
+app.use(cors())
 app.use(express.json());
 app.use("/todo", toDoRoutes);
 
