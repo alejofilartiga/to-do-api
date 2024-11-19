9e47a01e249ea8c61d11633b597614e95ce12173
Alejo <alejofilartiga@gmail.com>
2024-11-19 19:35:36 -0300

    Fix

--- index.ts
+++ index.ts
@@ -2,17 +2,21 @@ import express from "express";
 import { connectDB } from "./database/config";
 import toDoRoutes from './routes/toDoRoutes';
 import dotenv from "dotenv";
-// import cors from "cors"
-
+import cors from "cors"
+const corsConfig = {
+  origin:"*",
+  credential:true,
+  methods: ["GET", "POST", "PATCH", "DELETE"],
+}
 dotenv.config();
 
 const app = express();
 
 connectDB();
 
-// app.use(cors())
+app.use(cors(corsConfig))
 app.use(express.json());
-app.use("/", toDoRoutes);
+app.use("/todo", toDoRoutes);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
